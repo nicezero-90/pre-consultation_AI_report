f383fafec3be2e5ead21c90e673dcd0943e1e2e7
nicezero-90 <jolin@cofit.me>
2025-03-24 02:11:12 +0000

    Updated Colab notebook

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,1 @@
 # 空檔案，標記app為Python套件
-%%writefile app/api/__init__.py
-# 空檔案，標記api為Python套件
-%%writefile app/core/__init__.py
-# 空檔案，標記core為Python套件
-%%writefile app/core/processors/__init__.py
-# 空檔案，標記processors為Python套件
---
+++ app/core/processors/__init__.py
@@ -0,0 +1,1 @@
+# 空檔案，標記processors為Python套件
